d57c555560bdc07c739e96a00ab7eee76acdc3e5
mingli202 <vincentmingli@gmail.com>
2026-09-10 10:54:55 -0400

    home directory depends on machine

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,7 +19,11 @@ in
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "vincentliu";
-  home.homeDirectory = "/Users/vincentliu";
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin then
+      "/Users/${config.home.username}"
+    else
+      "/home/${config.home.username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
